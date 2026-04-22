5709dd83fa6ca011931fabf9c59e2d0da3c824fe
NgokChou <dnccrack76@gmail.com>
2026-04-22 07:09:10 +0700

    Update stock.ts

--- src/app/services/stock.ts
+++ src/app/services/stock.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of, throwError } from 'rxjs';
+import { Stock } from '../model/stock';
 @Injectable({
   providedIn: 'root',
 })
-export class Stock {}
+export class Stock {
+   private stocks: Stock[] = [
+    new Stock('Test Stock Company', 'TSC', 85, 45, 'NYSE'),
+    new Stock('Second Stock Company', 'SSC', 10, 20, 'NSE'),
+    new Stock('Third Stock Company', 'THC', 500, 700, 'NYSE'),
+  ];
+
+  constructor() {
+    getStocks(): Observable<Stock[]> {
+    return of(this.stocks);   // of() bọc giá trị thường thành Observable
+  }
+  }
+}
