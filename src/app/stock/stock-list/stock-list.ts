41021101f5a90ef0d6b60975e24f9b44b2c78ba4
NgokChou <dnccrack76@gmail.com>
2026-05-05 23:23:27 +0700

    Update stock-list.ts

--- src/app/stock/stock-list/stock-list.ts
+++ src/app/stock/stock-list/stock-list.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { StockItem } from '../stock-item/stock-item';
 import { StockService } from '../../services/stock';
 import { Stock } from '../../model/stock';
@@ -7,25 +8,41 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-stock-list',
-  imports: [CommonModule, StockItem],
+  imports: [CommonModule, FormsModule, StockItem],
   templateUrl: './stock-list.html',
   styleUrl: './stock-list.css',
 })
 export class StockList implements OnInit, OnDestroy {
 
-  stocks: Stock[] = [];
+  stocks: Stock[] = []; //danh sách hiển thị ở con, lấy từ Service
+  keyword: string = ''; //từ khóa tìm kiếm
   private sub!: Subscription;
 
   constructor(private stockService: StockService) {}
 
   ngOnInit(): void {
-    this.sub = this.stockService.getStocks().subscribe(data => {
-      this.stocks = data; // ← lấy từ Service
+    this.sub = this.stockService.stocks$.subscribe(data => {
+      this.stocks = this.filterStocks(data); // lọc lại mỗi khi data thay đổi
     });
   }
 
   ngOnDestroy(): void {
-    this.sub.unsubscribe();
+    this.sub.unsubscribe(); 
+  }
+
+  onSearch(): void {
+    this.sub = this.stockService.stocks$.subscribe(data => {
+      this.stocks = this.filterStocks(data); 
+    });
+  }
+
+  private filterStocks(data: Stock[]): Stock[] {
+    const kw = this.keyword.trim().toLowerCase();
+    if (!kw) return data; 
+    return data.filter(stock =>
+      stock.name.toLowerCase().includes(kw) ||
+      stock.code.toLowerCase().includes(kw)
+    );
   }
 
   handleDelete(code: string): void {
