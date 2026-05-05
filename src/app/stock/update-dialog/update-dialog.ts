27cad771e4a8086423917911b5eb657df90ed405
NgokChou <dnccrack76@gmail.com>
2026-05-06 01:06:38 +0700

    update

---
+++ src/app/stock/update-dialog/update-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-dialog',
+  imports: [],
+  templateUrl: './update-dialog.html',
+  styleUrl: './update-dialog.css',
+})
+export class UpdateDialog {}
