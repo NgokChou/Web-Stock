889fde9b42eb846f2e9ba13622f41749b903a221
NgokChou <dnccrack76@gmail.com>
2026-05-05 23:27:14 +0700

    update

---
+++ src/app/stock/detail-dialog/detail-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-dialog',
+  imports: [],
+  templateUrl: './detail-dialog.html',
+  styleUrl: './detail-dialog.css',
+})
+export class DetailDialog {}
