82fe7bc1e12c57759f22be2d11adc5e588d2ef41
NgokChou <dnccrack76@gmail.com>
2026-05-07 20:43:58 +0700

    Update stock.ts

--- src/app/services/stock.ts
+++ src/app/services/stock.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Stock } from '../model/stock';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StockService {
 
-  constructor() { }
+  private stocks: Stock[] = [
+    new Stock('Test Stock Company', 'TSC', 100, 120, 'NASDAQ'),
+    new Stock('Second Stock Company', 'SSC', 200, 180, 'NYSE'),
+    new Stock('Last Stock Company', 'LSC', 150, 140, 'NSE'),
+  ];
+
+  getStocks(): Stock[] {
+    return this.stocks;
+  }
+
+  createStock(stock: Stock) {
+    let foundStock = this.stocks.find(each => each.code === stock.code);
+    if (foundStock) {
+      return false; // Trùng mã, không thêm
+    }
+    this.stocks.push(stock);
+    return true; // Thêm thành công
+  }
+  toggleFavorite(stock: Stock) {
+    let foundStock = this.stocks.find(each => each.code === stock.code);
+    if (foundStock) {
+      foundStock.favorite = !foundStock.favorite;
+    }
+  }
 }
